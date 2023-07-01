a0d4aadb65c8e4cc874762033c66772a60df3417
Tucker Dickson <nathan-dickson@uiowa.edu>
2023-07-01 18:57:57 -0500

    Initial Commit

---
+++ SunShield/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SunShield
+//
+//  Created by Tucker Dickson on 7/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SunShield/SunShieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunShieldApp.swift
+//  SunShield
+//
+//  Created by Tucker Dickson on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SunShieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
